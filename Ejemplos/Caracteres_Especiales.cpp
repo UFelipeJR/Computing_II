ebd89a6ee2436b49814a2306164fc6b1a4388c59
UFelipeJR <jimenezramirezfelipe4@gmail.com>
2023-08-14 17:28:33 -0500

    Update Caracteres_Especiales.cpp
    Descripción

--- Ejemplos/Caracteres_Especiales.cpp
+++ Ejemplos/Caracteres_Especiales.cpp
@@ -1,9 +1,11 @@
+#Usar caracteres especiales en consola C++
+
 #include <iostream>
 #include <locale.h>
 using namespace std;
 
 int main(){
-    setlocale(LC_ALL, ""); // Configura la localización actual
+    setlocale(LC_ALL, ""); 
 
     wstring numero;
     wcout << L"Ingrese un número: "; 
